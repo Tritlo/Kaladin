4ed654120b9f933cce391b3870303b5d8db557f2
tritlo <mpg@mpg.is>
2014-02-10 22:16:12 +0000

    web code now nicer

--- web.js
+++ web.js
@@ -1,39 +1,24 @@
 function lex (){
     var t = $("#input").val();
-    console.log(t);
     simpl.setInput(t);
     var next = 0;
-    var output = [];
-    var matches = [];
+    var match = "";
+    var current;
+    var list;
+    $("#out").empty();
+    
     while(next !== 1){
 	this.yy = false;
 	next = simpl.lex();
 	if(next && next !== "T" && next !== "NL"){
-	    var match = simpl.match;
-	    output.push(next);
-	    matches.push(match);
-	}
-   }
-    $("#out").empty();
-    //$("#out").append("<ul class='list-group'>");
-    var current;
-    var list;
-    var currents =[];
-    for(var i=0; i < output.length;i++){
-	//if (i % 2 ==0){
+	    match = simpl.match;
 	    list = $("<ul class='list-group'>");
             current = $("<div class='col-sm-4'>");
-	    
-	 //   }
-	list.append("<li class='list-group-item'><div class='panel panel-primary'><div class='panel-heading'>"  + output[i] + "</div><div class='panel-body'>" + matches[i]  +"</div></div></li>");
-	//if (i% 2 ==1){
+	    list.append("<li class='list-group-item'><div class='panel panel-primary'><div class='panel-heading'>"  + next + "</div><div class='panel-body'>" +match  +"</div></div></li>");
 	    current.append(list);
-	    currents.push(current);
-	//}
-    }
-    for (cur in currents){
-	$("#out").append(currents[cur]);
-    }
+	   $("#out").append(current);
+	}
+   }
     
 }
 
